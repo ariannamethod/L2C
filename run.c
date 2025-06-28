0a89c646dd726b7d8754e03c6fc6a8b611f36593
Mirza-Samad-Ahmed-Baig <samadahmed87@gmail.com>
2025-06-28 18:41:54 +0500

    "refactor_cli_args"

--- run.c
+++ run.c
@@ -894,43 +894,47 @@ void error_usage() {
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char *argv[]) {
-
+void parse_cli_arguments(int argc, char *argv[], char **checkpoint_path, char **tokenizer_path, float *temperature, float *topp, int *steps, char **prompt, unsigned long long *rng_seed, char **mode, char **system_prompt) {
     // default parameters
-    char *checkpoint_path = NULL;  // e.g. out/model.bin
-    char *tokenizer_path = "tokenizer.bin";
-    float temperature = 1.0f;   // 0.0 = greedy deterministic. 1.0 = original. don't set higher
-    float topp = 0.9f;          // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
-    int steps = 256;            // number of steps to run for
-    char *prompt = NULL;        // prompt string
-    unsigned long long rng_seed = 0; // seed rng with time by default
-    char *mode = "generate";    // generate|chat
-    char *system_prompt = NULL; // the (optional) system prompt to use in chat mode
+    *checkpoint_path = NULL;  // e.g. out/model.bin
+    *tokenizer_path = "tokenizer.bin";
+    *temperature = 1.0f;   // 0.0 = greedy deterministic. 1.0 = original. don't set higher
+    *topp = 0.9f;          // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
+    *steps = 256;            // number of steps to run for
+    *prompt = NULL;        // prompt string
+    *rng_seed = 0; // seed rng with time by default
+    *mode = "generate";    // generate|chat
+    *system_prompt = NULL; // the (optional) system prompt to use in chat mode
 
     // poor man's C argparse so we can override the defaults above from the command line
-    if (argc >= 2) { checkpoint_path = argv[1]; } else { error_usage(); }
+    if (argc >= 2) { *checkpoint_path = argv[1]; } else { error_usage(); }
     for (int i = 2; i < argc; i+=2) {
         // do some basic validation
         if (i + 1 >= argc) { error_usage(); } // must have arg after flag
         if (argv[i][0] != '-') { error_usage(); } // must start with dash
         if (strlen(argv[i]) != 2) { error_usage(); } // must be -x (one dash, one letter)
         // read in the args
-        if (argv[i][1] == 't') { temperature = atof(argv[i + 1]); }
-        else if (argv[i][1] == 'p') { topp = atof(argv[i + 1]); }
-        else if (argv[i][1] == 's') { rng_seed = atoi(argv[i + 1]); }
-        else if (argv[i][1] == 'n') { steps = atoi(argv[i + 1]); }
-        else if (argv[i][1] == 'i') { prompt = argv[i + 1]; }
-        else if (argv[i][1] == 'z') { tokenizer_path = argv[i + 1]; }
-        else if (argv[i][1] == 'm') { mode = argv[i + 1]; }
-        else if (argv[i][1] == 'y') { system_prompt = argv[i + 1]; }
+        if (argv[i][1] == 't') { *temperature = atof(argv[i + 1]); }
+        else if (argv[i][1] == 'p') { *topp = atof(argv[i + 1]); }
+        else if (argv[i][1] == 's') { *rng_seed = atoi(argv[i + 1]); }
+        else if (argv[i][1] == 'n') { *steps = atoi(argv[i + 1]); }
+        else if (argv[i][1] == 'i') { *prompt = argv[i + 1]; }
+        else if (argv[i][1] == 'z') { *tokenizer_path = argv[i + 1]; }
+        else if (argv[i][1] == 'm') { *mode = argv[i + 1]; }
+        else if (argv[i][1] == 'y') { *system_prompt = argv[i + 1]; }
         else { error_usage(); }
     }
 
     // parameter validation/overrides
-    if (rng_seed <= 0) rng_seed = (unsigned int)time(NULL);
-    if (temperature < 0.0) temperature = 0.0;
-    if (topp < 0.0 || 1.0 < topp) topp = 0.9;
-    if (steps < 0) steps = 0;
+    if (*rng_seed <= 0) *rng_seed = (unsigned int)time(NULL);
+    if (*temperature < 0.0) *temperature = 0.0;
+    if (*topp < 0.0 || 1.0 < *topp) *topp = 0.9;
+    if (*steps < 0) *steps = 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    parse_cli_arguments(argc, argv, &checkpoint_path, &tokenizer_path, &temperature, &topp, &steps, &prompt, &rng_seed, &mode, &system_prompt);
 
     // build the Transformer via the model .bin file
     Transformer transformer;
